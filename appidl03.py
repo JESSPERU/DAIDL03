bb773ee8375507a934c7aa6e683d4441337f5e6c
JESSPERU <i2512170@continental.edu.pe>
2025-03-15 15:30:25 -0500

    Mostrando la información READ y elimiando los comprobantes DELETE

--- appidl03.py
+++ appidl03.py
@@ -12,18 +12,19 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 #Titulo de la app
 st.title("Registro de comprobantes de pago - SUNAT")
 
-#Solicitar los datos del comprobante de pagos"
+#CREATE
+#Solicitar los datos del comprobante de pagos" 
 st.header("Datos del comprobante de pago")
 
 fecha_emision = st.date_input("Fecha de emisión")
 tipo = st.selectbox("Tipo",["Factura","Boleta","Recibo","Otros"])
 serie = st.text_input("Serie")
 numero = st.text_input("Número")
-monto = st.number_input("Monto total")
+monto = st.number_input("Monto total", min_value=0.01)
 ruc = st.text_input("RUC", max_chars=11)
 concepto = st.text_area("Descripción")
 
-"Subir la información a la tabla en Supabase"
+#Subir la información a la tabla en Supabase
 if st.button("Registrar Comprobante"):
     if fecha_emision and tipo and serie and numero and monto and ruc and concepto:
         data = {
@@ -41,3 +42,24 @@ if st.button("Registrar Comprobante"):
         st.rerun()
     else:
         st.warning("Todos los campos son obligatorios")
+
+#READ
+# MOSTRAR COMPROBANTES REGISTRADO
+
+st.header("Mostrar los comprobantes de pago registrados")
+comprobantes = supabase.table("comprobantes").select("*").execute()
+
+if comprobantes.data:
+    for cdp in comprobantes.data:
+        with st.expander(f"{cdp['tipo']} {cdp['serie']} - {cdp['numero']} "):
+            st.write(f"Fecha:{cdp['fecha_emision']}")
+            st.write(f"Monto:S/{cdp['monto']}")
+            st.write(f"RUC:{cdp['ruc']}")
+            st.write(f"Descripción:{cdp['concepto']}")
+
+            # Botón para eliminar comprobante
+            if st.button(f"Eliminar comprobante", key=f"del_{cdp['id']}"):
+                supabase.table("comprobantes").delete().eq("id", cdp['id']).execute()
+                st.success(f"Comprobante {cdp['serie']}-{cdp['numero']} eliminado correctamente")
+                time.sleep(2)
+                st.rerun()
